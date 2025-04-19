4151ff779d1a633a7ed172fe7bd35c8003b3e1ef
nnul <salahxd99@gmail.com>
2025-04-19 23:18:46 +0100

    fixed some issues, the system extracts all text from the imge, you can configuere the system prompt to get you whatever you want from the captured image(see Scanning.kt), you also have to change the api key from BuildConfig, set it to your gemini api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout.core)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.generativeai)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/expensetracker/BuildConfig.kt
+++ app/src/main/java/com/example/expensetracker/BuildConfig.kt
@@ -1,4 +1,7 @@
 package com.example.expensetracker
 
-class BuildConfig {
-}
+data class BuildConfig(
+    public val apiKey:String = "AIzaSyB04Rd-g4N0Cio9NX1ok9w0j9QjL_6mMgc" //هذا استبدله ب gemini api key خاصبة بيك انت
+                                                                        //تقدر تجيب مفتاح مجانا ادخل goodle ai studio يمدولك مفتاح باطل
+                                                                        //https://aistudio.google.com/apikey
+)
